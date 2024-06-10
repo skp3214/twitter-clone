1eb640ecae854e7ccf70c862760f581e429e66e7
SACHIN PRAJAPATI <skprajapati3214@gmail.com>
2024-06-10 21:37:09 +0530

    cors setting

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,10 @@ connectToMongo();
 
 const app = express();
 const PORT = config.PORT;
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
